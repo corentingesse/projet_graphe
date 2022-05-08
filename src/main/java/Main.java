05ce5281569f2d77eaa5b8b85f13bdd4084c08ed
Nicolas DE SAINT JEAN <nicolas.de-saint-jean@etu.univ-lyon1.fr>
2022-05-08 13:53:51 +0200

    ajout fichiers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("src/main/resources/test.csv");
+        File file = new File("src/main/resources/graphe.csv");
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
 
@@ -11,7 +11,7 @@ public class Main {
         while ((c = br.read()) != -1) {
             char ch = (char) c;
 
-            System.out.println(c);
+            System.out.print(ch);
         }
     }
 }
